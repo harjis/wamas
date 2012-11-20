52096e9034a0ee4803bd5e13a449767a77306639
Joonas Harjukallio <joonas@devlab.fi>
2012-11-20 20:09:06 +0200

    Project base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wamas::Application.config.secret_token = '51e8dcc442f711a419d722bf79c85eae98565c161fb123c95ba9ad47b076b10d5e56cdf8ae7edc3814e03bd06d16bfae51428bf60fa17c1edc78688dcae64f9d'
